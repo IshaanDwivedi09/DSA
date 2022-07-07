1b409ae14ed956b67ba0988b9e5b8bdd9d82049f
Ishaan Dwivedi <74087061+ishaandwivedi9101@users.noreply.github.com>
2022-07-08 00:16:56 +0530

    Update longestCommonSubsequence.cpp

--- longestCommonSubsequence.cpp
+++ longestCommonSubsequence.cpp
@@ -1,14 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/* Returns length of LCS for X[0..m-1], Y[0..n-1] */
 int lcs(char *X, char *Y, int m, int n)
 {
-    // intitalizing a matrix of size (m+1)*(n+1)
     int L[m + 1][n + 1];
-
-    /* Following steps build L[m+1][n+1] in bottom up fashion. Note
-        that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
     for (int i = 0; i <= m; i++)
     {
         for (int j = 0; j <= n; j++)
@@ -24,11 +19,10 @@ int lcs(char *X, char *Y, int m, int n)
         }
     }
 
-    // L[m][n] contains length of LCS for X[0..n-1] and Y[0..m-1]
     return L[m][n];
 }
 
-// Driver program to test above function
+// Driver code
 int main()
 {
 
